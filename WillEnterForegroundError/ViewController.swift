1c753ae055a0fc80b9279480cee2e75b81e6c9e6
akasaaa <ichimots@gmail.com>
2018-09-22 15:29:51 +0900

    add

---
+++ WillEnterForegroundError/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WillEnterForegroundError
+//
+//  Created by 赤迫亮太 on 2018/09/22.
+//  Copyright © 2018年 akasaaa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
